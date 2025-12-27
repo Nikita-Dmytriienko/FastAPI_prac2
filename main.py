7ba41fa9a4c77c1bf75612c20b0463c540755489
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-27 19:43:08 +0200

    feat: add /users/admin endpoint returning admin message and /users/{name} dynamic user endpoint

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import mimetypes
-from fastapi import FastAPI, Response
-from fastapi.responses import JSONResponse, PlainTextResponse, HTMLResponse, FileResponse
+from fastapi import FastAPI
+
+
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return FileResponse("index.html",
-                        filename="mainpage.html",
-                        media_type="application/octet-stream")
+
+@app.get("/users/admin")
+def admin():
+    return {"message": "Admin"}
     
-@app.get("/users/{name}/{age}")
-def users(name, age):
-    return {"user_name": name, "user_age": age}
+@app.get("/users/{name}")
+def users(name):
+    return {"user_name": name}
     
