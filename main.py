ba5850ff3e286331ac9b2672430cbe7e8addef5a
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-27 20:06:56 +0200

    feat: add /users/admin endpoint returning admin message and /users/{name} dynamic user endpoint and /users/{id] dynamic user endpoint and /users/{phone} endpoint with pattern

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 
 app = FastAPI()
@@ -8,11 +8,19 @@ app = FastAPI()
 def admin():
     return {"message": "Admin"}
     
-@app.get("/users/{name}")
-def users(name: str):
+@app.get("/users/name/{name}")
+def users(name: str = Path(min_length=3, max_length=16)):
     return {"user_name": name}
 
-@app.get("/users/{id}")
+@app.get("/users/age/{age}")
+def users(age: int = Path(ge = 18, lt = 100)):
+    return {"user_age": age}
+          
+@app.get("/users/id/{id}")
 def users(id: int):
     return {"user_id": id}
+
+@app.get("/users/phone/{phone}")
+def users(phone:str  = Path(pattern=r"^\d{11}$")):
+    return {"phone": phone}
     
