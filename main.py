6d04f5354c465b9063bdab16a54e3bce0b1c78e2
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-27 19:45:55 +0200

    feat: add /users/admin endpoint returning admin message and /users/{name} dynamic user endpoint and /users/{id] dynamic user endpoint

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def admin():
     return {"message": "Admin"}
     
 @app.get("/users/{name}")
-def users(name):
+def users(name: str):
     return {"user_name": name}
+
+@app.get("/users/{id}")
+def users(id: int):
+    return {"user_id": id}
     
