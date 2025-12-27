acf94990353a292815c0e84c0c40241cd8fb49ae
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2025-12-27 18:39:57 +0200

    feat: add /html endpoint returning HTMLResponse and /text endpoint returning PlainTextResponse

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from fastapi.responses import JSONResponse, PlainTextResponse, HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def root():
+@app.get("/html", response_class= HTMLResponse)
+def root_html():
     data = "<h1> NEKETS <h1>"
     return HTMLResponse(content=data)
+
+@app.get("/text", response_class=PlainTextResponse)
+def root_text():
+    data = "<NEKETS>"
+    return PlainTextResponse(content=data)
