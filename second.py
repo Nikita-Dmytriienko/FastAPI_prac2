e9ec42e55ca33a5c2631b02ee647b18d2553af79
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-28 12:38:18 +0200

    feat: add age query param with validation (ge=18, lt=100)

--- second.py
+++ second.py
@@ -3,5 +3,6 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 @app.get("/users")
-def users(name: str = Query(min_length=3, max_length=20)):
-    return {"name": name}
+def users(name: str = Query(min_length=3, max_length=20),
+          age: int = Query(ge=18, lt = 100)):
+    return {"user_name": name, "user_age":age}
