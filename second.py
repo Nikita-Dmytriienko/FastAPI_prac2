a27f60be8b1181c2a5715ff19955db26d0be6deb
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-28 12:53:38 +0200

    feat: add phone query param with regex validation for 10 digits

--- second.py
+++ second.py
@@ -3,6 +3,5 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 @app.get("/users")
-def users(name: str = Query(min_length=3, max_length=20),
-          age: int = Query(ge=18, lt = 100)):
-    return {"user_name": name, "user_age":age}
+def users_phone_number(phone: str = Query(pattern=r"^\d{10}$")):
+    return {"user_phone_number": phone}
