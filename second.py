6f3311ef02eeac753c4dc2ddeafdb584253a4105
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-28 13:10:32 +0200

    feat: add /users/{name} endpoint with path param validation and age query param

--- second.py
+++ second.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 
 app = FastAPI()
 
-@app.get("/users")
-def users(people: list[str] = Query()):
-    return {"people": people}
+@app.get("/users/{name}")
+def users(name: str = Path(min_length=3, max_length=20),
+          age: int = Query(ge=18, lt=100)):
+    return {"name": name, "age": age}   
