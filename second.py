610242c8f8bcb037d14027ddc0b66b725a80f20d
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-28 13:01:53 +0200

    feat: add /users endpoint accepting multiple name query params as list

--- second.py
+++ second.py
@@ -3,9 +3,5 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 @app.get("/users")
-def users(name: str | None = Query(default = None,min_length=2)):
-    if name == None:
-        return {"name": "Undefined"}
-    else:
-        return {"name": name}
-    
+def users(people: list[str] = Query()):
+    return {"people": people}
