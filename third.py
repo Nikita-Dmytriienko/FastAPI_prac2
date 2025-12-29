ada8baad9be2d560c35522fc8b375c69eae0845e
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2025-12-29 07:58:41 +0200

    feat: enable HTML mode for main page

--- third.py
+++ third.py
@@ -3,4 +3,4 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="public",html=True))
+app.mount("/", StaticFiles(directory="public",html=True))
