8ccf69e5d64be4acca9650b8c54f57a67a7c14db
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-01 17:24:49 +0200

    feat: add validation to name (min_length=3, max_length=50) and age (ge=18, lt=100) in /hello

--- third.py
+++ third.py
@@ -8,7 +8,8 @@ def root():
     return FileResponse("public/index.html")
 
 @app.post("/hello")
-def hello(name = Body(embed=True), age = Body(embed=True)):
+def hello(name = Body(embed=True, min_length=3, max_length=50),
+          age = Body(embed=True, ge= 18, lt=100)):
     # name = data ["name"]
     # age = data ["age"]
     return {"message": f"{name}, your age = {age} "}
