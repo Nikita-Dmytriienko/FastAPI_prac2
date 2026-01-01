226d97cf9f28a463b7e4c46d0e57d2e78eeba8fc
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-01 17:55:55 +0200

    feat: make age optional in Person model with conditional greeting

--- test4.py
+++ test4.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Person(BaseModel):
     name: str
-    age: int
+    age: Optional[int] = None
     
 app = FastAPI()
 
@@ -12,6 +13,10 @@ app = FastAPI()
 def root():
     return FileResponse("public/index.html")
 
+
 @app.post("/hello")
 def hello(person: Person):
-    return {"message": f"Hi, {person.name}, your age is {person.age}"}
+    if person.age == None:
+        return {"message": f"Hi, {person.name}"}
+    else:
+        return {"message": f"Hi, {person.name}, your age is {person.age}"}
