b71d2daef3909f36a0e941fe38ffb0816bbf6d02
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2026-01-02 18:55:01 +0200

    refactor: refactoring

--- simple_api.py
+++ simple_api.py
@@ -11,6 +11,7 @@ class Person:
 
 people = [Person("Tom", 38), Person("Bob", 42), Person("Sam", 28)]
 
+
 def find_person(id):
     for person in people:
         if person.id == id:
@@ -18,15 +19,19 @@ def find_person(id):
 
     return None
 
+
 app = FastAPI()
+
 
 @app.get("/")
 async def main():
     return FileResponse("public/index.html")
 
+
 @app.get("/api/users")
 def get_people():
     return people
+
 
 @app.get("/api/users/{id}")
 def get_person(id):
@@ -41,11 +46,13 @@ def get_person(id):
 
     return person
 
+
 @app.post("/api/users")
 def create_person(data = Body()):
     person = Person(data["name"], data["age"])
     people.append(person)
     return person
+
 
 @app.put("/api/users")
 def edit_person(data = Body()):
@@ -58,6 +65,7 @@ def edit_person(data = Body()):
     person.age = data["age"]
     person.name = data["name"]
     return person
+
 
 @app.delete("/api/user/{id}")
 def delete_person(id):
