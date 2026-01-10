e33ff6fad30e268130b27becc25336ccf389f052
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2026-01-10 20:12:43 +0200

    feat: add last_visit cookie reading in root endpoint

--- test/headers_prac.py
+++ test/headers_prac.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from datetime import datetime
+from fastapi import FastAPI, Cookie
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    now = datetime.now()
-    response = JSONResponse(content={"message": "cookies enabled"})
-    response.set_cookie(key="last_visit", value=now)
-    return response
+def root(last_visit: str | None = Cookie(default=None)):
+    if last_visit is None:
+        return {"message": "Its your first visit"}
+    else:
+        return {"last visit": last_visit}
