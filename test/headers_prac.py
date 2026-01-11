a8773e0c98b91919771fee6205d586b1c6d907a9
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2026-01-11 15:42:54 +0200

    feat: update form in index.html to support multiple languages inputs

--- test/headers_prac.py
+++ test/headers_prac.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
+
 app = FastAPI()
 
 @app.get("/")
@@ -7,6 +8,6 @@ def root():
     return FileResponse("public_2/index.html")
 
 @app.post("/postdata")
-def postdata(username: str = Form(min_length=2, max_length=20),
-             userage: int = Form(ge=18, lt=100)):
-    return {"name": username, "age": userage}
+def postdata(username: str = Form(),
+             languages: list = Form()):
+    return {"name": username, "languages": languages}
